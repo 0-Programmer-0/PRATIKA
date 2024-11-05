a465e4f55d08101e38222b2a3146c7bb4ba7ddd7
Nadiélly Santos <nadysantos35@gmail.com>
2024-11-04 22:27:30 -0300

    controller comunidade

---
+++ PBL_EC8/Controllers/ComunidadeController.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Pbl_EC8.Models;
+using PBL_EC8.Bll;
+using PBL_EC8;
+
+namespace PBL_EC8.Controllers;
+
+public class ComunidadeController: Controller
+{
+    private readonly ILogger<ComunidadeController> _logger;
+
+    // Construtor injetando tanto o ILogger quanto o UsuarioBll
+    public ComunidadeController(ILogger<ComunidadeController> logger)
+    {
+        _logger = logger;
+       
+    }
+
+   public IActionResult Comunidade()
+    {
+        return View("comunidade");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+}
